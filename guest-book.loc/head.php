76945aae69f751f15633a9a810db37258491379f
YanaMiroshnichenko <yyanam@yandex.ru>
2018-03-19 15:03:16 +0200

    add validator.min.js

--- guest-book.loc/head.php
+++ guest-book.loc/head.php
@@ -6,5 +6,6 @@
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
    	<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css"> 
   	<script src="bootstrap/js/bootstrap.min.js"></script> 
+  	<script src="validator.min.js"></script>
 </head>
 <body>
