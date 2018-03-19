1130de5dbffd1a18289bd53f5e84e2f4a40ed8e0
YanaMiroshnichenko <yyanam@yandex.ru>
2018-03-19 19:47:36 +0200

    good version registration

--- guest-book.loc/registration.php
+++ guest-book.loc/registration.php
@@ -1,63 +1,80 @@
 <?php
 	require_once('./bd.php');
-	$arrayName = [];
+	$arrayErrors = [];
 	$first_name =  '';
 	$last_name = '';
 	$email = '';
 	$password = '';
 	$second_password = '';
-	
+	$has_errors = FALSE;
 	if (isset($_POST) && !empty($_POST)) {
 		$first_name = preg_replace("/\s{2,}/",' ',trim($_POST['first-name']));
 		$last_name = preg_replace("/\s{2,}/",' ',trim($_POST['last-name']));
 		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
 		$second_password = trim($_POST['second-password']);
-		$arrayName['first-name'] = '';
+
+		$arrayErrors['first-name'] = '';
+		$arrayErrors['last-name'] = '';
+		$arrayErrors['email'] = '';
+		$arrayErrors['password'] = '';
+		$arrayErrors['second-password'] = '';
+		
 		if (empty($first_name) ) {
-			$arrayName['first-name'] = 'Введите имя!'; 
+			$arrayErrors['first-name'] = 'Введите имя!'; 
+			$has_errors = TRUE;
 		}
 
 		if (empty($last_name) ) { 
-			$arrayName['last-name'] = 'Введите фамилию!'; 
+			$arrayErrors['last-name'] = 'Введите фамилию!'; 
+			$has_errors = TRUE;
 		}
 
 		if (empty($email) ) {
-			$arrayName['email'] = 'Введите e-mail адрес!'; 
+			$arrayErrors['email'] = 'Введите e-mail адрес!'; 
+			$has_errors = TRUE;
 		}
 		if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$arrayName['wrong-email'] = 'Введите корректный e-mail адрес!'; 
+			$arrayErrors['wrong-email'] = 'Введите корректный e-mail адрес!'; 
+			$has_errors = TRUE;
 		}
 
 		if (empty($password) ) {
-			$arrayName['password'] = 'Введите пароль!'; 
+			$arrayErrors['password'] = 'Введите пароль!'; 
+			$has_errors = TRUE;
 		}
 
 		if (empty($second_password) ) {
-			$arrayName['second-password'] = 'Введите пароль ещё раз!';
+			$arrayErrors['second-password'] = 'Введите пароль ещё раз!';
+			$has_errors = TRUE;
 		}
 		if (!empty($second_password) && $password!=$second_password) {
-			$arrayName['wrong-password'] = 'Пароли не совпадают!';
+			$arrayErrors['wrong-password'] = 'Пароли не совпадают!';
+			$has_errors = TRUE;
 		}
 		if (!empty($password) && strlen($password) < 5) {
-			$arrayName['min-password'] = 'Введите не менее 5-ти символов!';
+			$arrayErrors['min-password'] = 'Введите не менее 5-ти символов!';
+			$has_errors = TRUE;
 		}
 		if (strlen($password) > 10) {
-			$arrayName['max-password'] = 'Максимальное количество символов - 10!';
+			$arrayErrors['max-password'] = 'Максимальное количество символов - 10!';
+			$has_errors = TRUE;
 		}
 		$query = "SELECT `email` FROM users WHERE email=`$email`";
 		$result = mysqli_query($link, $query);
 
 		if (!empty($result) && !empty($email)) {
-			$arrayName['exists-email'] = 'Такой e-mail адрес уже зарегистрирован!';
+			$arrayErrors['exists-email'] = 'Такой e-mail адрес уже зарегистрирован!';
+			$has_errors = TRUE;
 		}
-		if ($password==$second_password && empty($arrayName) ) {
+		if ($password==$second_password && $has_errors==FALSE) {
 			$hash_password = md5($password);
 			$hash_email = md5($email);
 			$query = "INSERT INTO `users`(`first_name`, `last_name`, `email`, `password`) VALUES 
 		    							('$first_name', '$last_name', '$hash_email', '$hash_password')";
 			mysqli_query($link, $query);
 			mysqli_close($link);
+			
 			header("Location: index.php");
 		}
 	}
@@ -69,8 +86,10 @@
 		<h2>Регистрация</h2>
 		<form class="form form-horizontal form-group " method="post" id="form">
 			<?php 
-				$has_error_class = isset($arrayName['first-name']) && !empty($arrayName['first-name']) ? 'has-error' : '';
-				$has_success_class = isset($arrayName['first-name']) && empty($arrayName['first-name']) ? 'has-success' : '';
+				$has_error_class = isset($arrayErrors['first-name']) && !empty($arrayErrors['first-name']) ? 'has-error' : '';
+				$has_success_class = isset($arrayErrors['first-name']) && empty($arrayErrors['first-name']) ? 'has-success' : '';
+				
+
 			?>
 			<div class="form-element form-group <?php echo $has_error_class; echo $has_success_class;?>">
 			 	<div class="col-sm-12">
@@ -79,30 +98,31 @@
 			</div>
 		    <span class="result_of_query">
 				<?php 
-			    	if(isset($arrayName['first-name']) && !empty($arrayName['first-name'])   ) {
-						echo $arrayName['first-name'];
+			    	if(isset($arrayErrors['first-name']) && !empty($arrayErrors['first-name'])) {
+						echo $arrayErrors['first-name'];
+						
 					}
 		    	?>
 	    	</span>
 	    	<?php 
-				$has_error_class = isset($arrayName['last-name']) && !empty($arrayName['last-name']) ? 'has-error' : '';
-				$has_success_class = isset($arrayName['last-name']) && empty($arrayName['last-name']) ? 'has-success' : '';
+				$has_error_class = isset($arrayErrors['last-name']) && !empty($arrayErrors['last-name']) ? 'has-error' : '';
+				$has_success_class = isset($arrayErrors['last-name']) && empty($arrayErrors['last-name']) ? 'has-success' : '';
 			?>
-		   	<div class="form-element form-group <?php echo $has_error_class; echo $has_success_class;?>">
+		   	<div class="form-element form-group <?php echo $has_error_class; echo $has_success_class; ?>">
 			 	<div class="col-sm-12">
 		    		<input class="form-control" placeholder="Фамилия" type="text" name="last-name" value="<?php echo $last_name; ?>">	
 		    	</div>
 			</div>
 			<span class="result_of_query">
 			    <?php 
-			    	if(isset($arrayName['last-name']) && !empty($arrayName['last-name']) ){
-						echo $arrayName['last-name'];
+			    	if(isset($arrayErrors['last-name']) && !empty($arrayErrors['last-name']) ){
+						echo $arrayErrors['last-name'];
 					}
 		    	?>
 	    	</span>
 	    	<?php 
-				$has_error_class = isset($arrayName['email']) && !empty($arrayName['email']) ? 'has-error' : '';
-				$has_success_class = isset($arrayName['email']) && empty($arrayName['email']) ? 'has-success' : '';
+				$has_error_class = isset($arrayErrors['email']) && !empty($arrayErrors['email']) ? 'has-error' : '';
+				$has_success_class = isset($arrayErrors['email']) && empty($arrayErrors['email']) ? 'has-success' : '';
 			?>
 		    <div class="form-element form-group <?php echo $has_error_class; echo $has_success_class;?>">
 			 	<div class="col-sm-12">
@@ -111,20 +131,23 @@
 			</div>
 			<span class="result_of_query">
 		    	<?php 
-		    		if(isset($arrayName['email']) && !empty($arrayName['email']) ){
-						echo $arrayName['email'];
+		    		if(isset($arrayErrors['email']) && !empty($arrayErrors['email']) ){
+						echo $arrayErrors['email'];
+						
 					}
-					if(isset($arrayName['wrong-email']) && !empty($arrayName['wrong-email']) ){
-						echo $arrayName['wrong-email'];
+					if(isset($arrayErrors['wrong-email']) && !empty($arrayErrors['wrong-email']) ){
+						echo $arrayErrors['wrong-email'];
+						$has_errors = TRUE;
 					}
-					if(isset($arrayName['exists-email']) && !empty($arrayName['exists-email']) ){
-						echo $arrayName['exists-email'];
+					if(isset($arrayErrors['exists-email']) && !empty($arrayErrors['exists-email']) ){
+						echo $arrayErrors['exists-email'];
+						
 					}
 				?>
 	    	</span>
 	    	<?php 
-				$has_error_class = isset($arrayName['password']) && !empty($arrayName['password']) ? 'has-error' : '';
-				$has_success_class = isset($arrayName['password']) && empty($arrayName['password']) ? 'has-success' : '';
+				$has_error_class = isset($arrayErrors['password']) && !empty($arrayErrors['password']) ? 'has-error' : '';
+				$has_success_class = isset($arrayErrors['password']) && empty($arrayErrors['password']) ? 'has-success' : '';
 			?>
 		    <div class="form-element form-group <?php echo $has_error_class; echo $has_success_class;?>">
 			 	<div class="col-sm-12">
@@ -133,20 +156,23 @@
 			</div>
 			<span class="result_of_query">
 		    	<?php 
-			    	if(isset($arrayName['password']) && !empty($arrayName['password']) ){
-						echo $arrayName['password'];
+			    	if(isset($arrayErrors['password']) && !empty($arrayErrors['password']) ){
+						echo $arrayErrors['password'];
+						
 					}
-					if(isset($arrayName['min-password']) && !empty($arrayName['min-password']) ){
-						echo $arrayName['min-password'];
+					if(isset($arrayErrors['min-password']) && !empty($arrayErrors['min-password']) ){
+						echo $arrayErrors['min-password'];
+						
 					}
-					if(isset($arrayName['max-password']) && !empty($arrayName['max-password']) ){
-						echo $arrayName['max-password'];
+					if(isset($arrayErrors['max-password']) && !empty($arrayErrors['max-password']) ){
+						echo $arrayErrors['max-password'];
+						
 					}
 		    	?>
 		    </span>
 		    <?php 
-				$has_error_class = isset($arrayName['second-password']) && !empty($arrayName['second-password']) ? 'has-error' : '';
-				$has_success_class = isset($arrayName['second-password']) && empty($arrayName['second-password']) ? 'has-success' : '';
+				$has_error_class = isset($arrayErrors['second-password']) && !empty($arrayErrors['second-password']) ? 'has-error' : '';
+				$has_success_class = isset($arrayErrors['second-password']) && empty($arrayErrors['second-password']) ? 'has-success' : '';
 			?>
 		    <div class="form-element form-group <?php echo $has_error_class; echo $has_success_class;?>">
 			 	<div class="col-sm-12">
@@ -155,11 +181,13 @@
 			</div>
 			<span class="result_of_query">
 		    	<?php 
-			    	if(isset($arrayName['second-password']) && !empty($arrayName['second-password']) ) {
-						echo $arrayName['second-password'];
+			    	if(isset($arrayErrors['second-password']) && !empty($arrayErrors['second-password']) ) {
+						echo $arrayErrors['second-password'];
+						
 					}
-					if(isset($arrayName['wrong-password']) && !empty($arrayName['wrong-password']) ) {
-						echo $arrayName['wrong-password'];
+					if(isset($arrayErrors['wrong-password']) && !empty($arrayErrors['wrong-password']) ) {
+						echo $arrayErrors['wrong-password'];
+						
 					}
 				?>
 			</span>
